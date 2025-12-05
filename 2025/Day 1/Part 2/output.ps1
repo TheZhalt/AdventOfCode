6236d2136dff920ad7c5e7ce43c35411ba23ad71
TheZhalt <jonas.w.nobel@gmail.com>
2025-12-05 15:35:38 +0100

    Day 1 Part 2 commit

---
+++ 2025/Day 1/Part 2/output.ps1
@@ -0,0 +1,26 @@
+$TextInput = @()
+$GetLocation = Get-Location
+$Path = -join ($GetLocation, "\2025\Day 1\Part 1\input.txt")
+$TextInput = get-content $Path
+$ZeroCounter = 0
+$DialRange = @(0..99)
+$DialLocation = $DialRange[50]
+$DialRotation = 0
+
+"Dial starts pointing at $DialLocation"
+
+foreach ($Line in $TextInput) {
+    
+    $DialRotation = $Line.Replace("L", "-").Replace("R", "+")
+    $DialLocation = $DialRange[($DialLocation + $DialRotation) % 100]
+    "The dial is rotated $Line to point at $DialLocation"
+
+    if ($DialLocation -eq 0) {
+        $ZeroCounter++
+    }
+}
+"Amount of times that the dial passed or ended on zero: $ZeroCounter"
+
+
+$FullRotations = [math]::Floor($DialRotation / $DialRange.Length)
+$DialTest = $DialRange[($DialLocation + 500) % 100]
