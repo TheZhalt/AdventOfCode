eac1952df8fd6677c34283fe8cb5343169218618
TheZhalt <jonas.w.nobel@gmail.com>
2026-01-06 16:37:16 +0100

    updated day 1 part 2 (still fails) - and finished day 1 part 1

--- 2025/Day 1/Part 2/output.ps1
+++ 2025/Day 1/Part 2/output.ps1
@@ -1,26 +1,80 @@
+#### NOT GIVING CORRECT RESULT ####
+
 $TextInput = @()
 $GetLocation = Get-Location
-$Path = -join ($GetLocation, "\2025\Day 1\Part 1\input.txt")
+$Path = -join ($GetLocation, "\2025\Day 1\Part 2\input.txt")
 $TextInput = get-content $Path
 $ZeroCounter = 0
 $DialRange = @(0..99)
 $DialLocation = $DialRange[50]
+#$DialLocation = 0
 $DialRotation = 0
 
 "Dial starts pointing at $DialLocation"
 
 foreach ($Line in $TextInput) {
     
-    $DialRotation = $Line.Replace("L", "-").Replace("R", "+")
-    $DialLocation = $DialRange[($DialLocation + $DialRotation) % 100]
-    "The dial is rotated $Line to point at $DialLocation"
+    $FullRotations = 0
+    $DialRotation = $Line.Replace("L", "-").Replace("R", "")
+    "DialLocation: $DialLocation"
+    "DialRotation: $DialRotation"
+    $FullRotations = ($DialLocation + $DialRotation) / $DialRange.Length
+    "Full rotations: $FullRotations"
+    if ($FullRotations -le -1) {
+        "Test-1"
+        $FullRotations = [math]::Abs([math]::Ceiling(($FullRotations)))
+    }
+    elseif ($FullRotations -lt 0) {
+        "Test-2"
+        if ($DialLocation -ne 0) {
+            $FullRotations = [math]::Abs([math]::Floor(($FullRotations)))
+        }
+    }
 
-    if ($DialLocation -eq 0) {
+    $DialLocation = $DialRange[($DialLocation + $DialRotation) % 100]
+
+    if ($FullRotations -eq 0) {
         $ZeroCounter++
     }
+    "Full rotations: $FullRotations"
+    if ($FullRotations -gt 0) {
+        "Test-3"
+        $FullRotations = [math]::Floor($FullRotations)
+        if ($DialLocation -eq 0) {
+            "Test-3-5"
+            #$FullRotations++
+            if ($DialRotation -lt 0 -and $FullRotations -eq 1) {
+                "Test-3-5-1"
+                $FullRotations++
+            }
+            #if ($DialRotation -gt 0 -and $FullRotations -gt 1) {
+            #    "Test-3-5-3"
+            #    $FullRotations--
+            #}
+        }
+        "The dial is rotated $Line to point at $DialLocation and went to zero, $FullRotations time(s)"
+        $ZeroCounter += $FullRotations
+        
+    }
+    else {
+        "The dial is rotated $Line to point at $DialLocation"
+    }
+
+    "Zero counter: $ZeroCounter"
+
+    
+    #$FullRotations
+
 }
-"Amount of times that the dial passed or ended on zero: $ZeroCounter"
+"Amount of times that the dial passed or ended on zero: $ZeroCounter `n"
 
 
-$FullRotations = [math]::Floor($DialRotation / $DialRange.Length)
-$DialTest = $DialRange[($DialLocation + 500) % 100]
+#
+######
+#$DialLocation = 0
+#$FullRotations = -5
+#"Full rotations: $FullRotations"
+#if ($FullRotations -le -1) {
+#    $FullRotations = [math]::Abs([math]::Ceiling(($FullRotations)))
+#}
+#"Full rotations: $FullRotations"
---
+++ 2025/Day 2/Part 1/output.ps1
@@ -0,0 +1,29 @@
+$TextInput = @()
+$GetLocation = Get-Location
+$Path = -join ($GetLocation, "\2025\Day 2\Part 1\input.txt")
+$InputArray = @(get-content $Path -Delimiter ",")
+$InvalidNumberArray = @()
+$InvalidNumberSum = 0
+
+foreach ($ID in $InputArray) {
+    #$RangeStart
+    #$RangeEnd
+    $RangeStart = [Long]$ID.Split("-")[0]
+    $RangeEnd = [Long]$ID.Split("-")[1]
+    $InvalidCounter = 0
+    for ($Number = $RangeStart; $Number -le $RangeEnd; $Number++) {
+        $NumberString = [string]$Number
+        $mid = $NumberString.Length / 2
+        $first = $NumberString.Substring(0, $mid)
+        $second = $NumberString.Substring($mid)
+        if ($first -eq $second) {
+            $InvalidCounter++
+            $InvalidNumberArray += $Number
+        }
+    }
+}
+foreach ($InvalidNumber in $InvalidNumberArray) {
+    $InvalidNumberSum += $InvalidNumber
+}
+
+"Adding up all the invalid IDs gives the sum: $InvalidNumberSum"
